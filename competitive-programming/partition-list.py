e7f6d89abc755d29e6348421c65af61f6454083b
amanuelmandefro2 <128055581+amanuelmandefro2@users.noreply.github.com>
2024-01-25 14:45:40 +0300

    Added solution for Partition List

---
+++ competitive-programming/partition-list.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        left_tail, right_tail = ListNode(), ListNode()
+        head_left, head_right = left_tail, right_tail
+        
+        while head:
+            if head.val < x:
+                left_tail.next = head
+                left_tail = left_tail.next
+            else:
+                right_tail.next = head
+                right_tail = right_tail.next
+            head = head.next
+
+        # left.next = head_right.next  
+        right_tail.next = None
+        left_tail.next = head_right.next
+
+
+        return head_left.next                                
+        
