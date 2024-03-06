ce01de0fc93479652e3dd338c041654dadef820c
theamanomr <128055581+amanuelmandefro2@users.noreply.github.com>
2024-03-06 09:26:10 +0300

    Add solution for Power of Three

---
+++ leetcode/power-of-three.py
@@ -0,0 +1,11 @@
+from math import floor, ceil, log
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n==1:
+            return True
+        if n < 1:
+            return False
+        return  self.isPowerOfThree(n/3)       
+
+        
+        
